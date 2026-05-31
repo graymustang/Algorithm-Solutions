610509e0979ad952fcab077d387e1e977a87c2dc
Bjorn <graybjo0@byu.edu>
2026-05-30 20:07:07 -0600

    add palindrome-number algorithm

---
+++ palindrome-number/brute-force.js
@@ -0,0 +1,23 @@
+function isPalindrome (string){
+    const stack = []
+
+    for (const char of string) {
+        stack.push(char)
+    }
+
+    for(const char of string){
+        if (char !== stack.pop()){
+            return false
+        }
+    }
+    return true
+}
+
+function main(){
+    const string = "racecar"
+    console.log(isPalindrome(string))
+    console.log(isPalindrome("hello"))
+    console.log(isPalindrome("abba"))
+}
+
+main()
